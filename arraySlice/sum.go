1e3d43613463bc47b6269346280f82927fc78ae2
Luan Lima <luan@yata.dev>
2021-02-13 17:00:04 -0300

    transformacao do array em slices com isso o array vira dinamico

--- arraySlice/sum.go
+++ arraySlice/sum.go
@@ -1,15 +1,11 @@
 package arrayslice
 
 // Sum Soma os valores do array
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 
 	return sum
-}
-
-func main() {
-	Sum([5]int{1, 2, 3, 4, 5})
 }
--- arraySlice/sum_test.go
+++ arraySlice/sum_test.go
@@ -3,12 +3,16 @@ package arrayslice
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	result := Sum(numbers)
-	expected := 15
+	t.Run("Array Dinâmico", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
-	if result != expected {
-		t.Errorf("Expected %d, Result %d, Data %v", expected, result, numbers)
-	}
+		result := Sum(numbers)
+		expected := 6
+
+		if result != expected {
+			t.Errorf("Expected %d, Result %d, Data %v", expected, result, numbers)
+		}
+	})
+
 }
