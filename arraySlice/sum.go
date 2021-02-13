d3748ecb8998a5da0e22845fdfb3d5dcb514f261
Luan Lima <luan@yata.dev>
2021-02-13 16:37:58 -0300

    somatorio de array fixo usado for e range

--- arraySlice/sum.go
+++ arraySlice/sum.go
@@ -3,9 +3,13 @@ package arrayslice
 // Sum Soma os valores do array
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
+}
+
+func main() {
+	Sum([5]int{1, 2, 3, 4, 5})
 }
